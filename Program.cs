f79a7346e8f9448fe728c8dd2381ae6ad049f455
chldudms <yueun0337@gmail.com>
2025-05-29 09:45:28 +0900

    정적 생성자 호츌

--- Program.cs
+++ Program.cs
@@ -1,30 +1,41 @@
-﻿using System;
+﻿using ConsoleApp1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace PropExample32
 {
-   
     class Program
     {
-
         public static int classVar = 0;
+
         static void Main(string[] args)
         {
-            //클래스 변수 호출
+            // 클래스 변수 호출
             Program.classVar = -1;
-            //인스턴스 변수처럼 클래스 변수 사용 불가에 주의 (자바에서는 ok)
-            // Program p = new Program();
-            //p.classvar = 1; 
 
-            Console.WriteLine();
+            // 인스턴스 변수처럼 클래스 변수 사용 불가에 주의(자바에서는 OK)
+            //Program p = new Program();
+            //p.classVar = -3;
 
-            Box box1 = new Box(100,200);
+            // 생성자
+            // 기본(기정) 생성자 - 생성자를 하나라도 정의하면 기본 생성자를 따로 작성해줘야 함
+            //Product p = new Product();
+
+            // 팩토리 메서드 패턴 (생성자에 private를 사용하는 경우)
+            Product p = Product.GetInstance("아아", 1500);
+
+
+            // Property 실습
+            Box box1 = new Box(100, 200);
             box1.Width = -3;
             Console.WriteLine(box1.Width);
+
             Console.WriteLine(box1.Area);
+
+
         }
     }
 }
