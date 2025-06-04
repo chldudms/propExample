d24072dd6cdc3c972781e350a837a0965a869622
chldudms <yueun0337@gmail.com>
2025-06-04 14:04:09 +0900

    참조복사

--- Program.cs
+++ Program.cs
@@ -20,6 +20,19 @@ namespace PropExample32
             Console.WriteLine(item3.id);
             //item3.id = 100; // readonly 키워드가 지정된 인스턴스 변수는 수정 불가
 
+            // 참조 복사
+            Test test1 = new Test();
+            test1.value = 5;
+            Change(test1); // test1을 참조로 전달
+            Console.WriteLine(test1.value); // 20 출력
+        }
+        class Test
+        {
+            public int value = 10;
+        }
+        static void Change(Test input) //메인메서드오 ㅏ같은 위치 레퍼런스를 매개변수로 받음
+        {
+            input.value = 20; // 참조 복사로 인해 원본 값이 변경됨
         }
     }
 }
