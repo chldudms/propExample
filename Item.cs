2a9ddb75c2593b430dec4d71bcadf420ac3931b3
chldudms <yueun0337@gmail.com>
2025-06-04 13:55:37 +0900

    상수 readonly 키워드 비교 예제

---
+++ Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Item
+    {
+        private static int count;
+        public readonly int id;
+        public string name;
+        public int price;
+
+        public Item( string name, int price)
+        {
+            int id = count++;
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,39 +9,16 @@ namespace PropExample32
 {
     class Program
     {
-        public static int classVar = 0;
-
         static void Main(string[] args)
-        {
-            // 클래스 변수 호출
-            Program.classVar = -1;
-
-            // 인스턴스 변수처럼 클래스 변수 사용 불가에 주의(자바에서는 OK)
-            //Program p = new Program();
-            //p.classVar = -3;
-
-            // 생성자
-            // 기본(기정) 생성자 - 생성자를 하나라도 정의하면 기본 생성자를 따로 작성해줘야 함
-            //Product p = new Product();
-
-            // 팩토리 메서드 패턴 (생성자에 private를 사용하는 경우)
-            Product p = Product.GetInstance("아아", 1500);
-
-            //정적 생성자 예제
-            Console.WriteLine("첫 번째 위치");
-            Console.WriteLine(Sample.classVar);
-            Console.WriteLine("두 번째 위치");
-            Console.WriteLine(Sample.classVar);
-            Sample sample = new Sample();
-            Console.WriteLine("세 번째 위치");
-
-            // Property 실습
-            Box box1 = new Box(100, 200);
-            box1.Width = -3;
-            Console.WriteLine(box1.Width);
-
-            Console.WriteLine(box1.Area);
-
+        {   
+            //상수 readonly 키워드 비교 예제
+           Item item1 = new Item( "고구마", 1000);
+            Item item2 = new Item("감자", 2000);
+            Item item3 = new Item("옥수수", 3000);
+            Console.WriteLine(item1.id);
+            Console.WriteLine(item2.id);
+            Console.WriteLine(item3.id);
+            //item3.id = 100; // readonly 키워드가 지정된 인스턴스 변수는 수정 불가
 
         }
     }
